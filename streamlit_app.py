7a43c5e26d7b5d2149309f2f68371e73d3f2f3fe
PhienVuong <phienvuong.me@gmail.com>
2025-01-01 13:03:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ from xgboost import XGBRegressor
 
 file = st.file_uploader('Choose a CSV file', 'csv')
 if file:
-    data = pd.read_csv(file)
     data_df = pd.read_csv(file)
     
 data_df = data_df.drop(["Unnamed: 0"], axis=1)
